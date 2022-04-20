dd6e01efe4e9268f826b63ad8ed7bfb2a947e02f
liujunjie <liujunjiejob@126.com>
2022-04-20 23:01:57 +0800

    基础篇-03-SpringBoot入门案例（Idea联网版）

---
+++ springboot_01_01_quickstart/src/main/java/com/hy/controller/BookController.java
@@ -0,0 +1,16 @@
+package com.hy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//rest模式
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    @GetMapping
+    public String getBookById() {
+        System.out.println("springboot is running");
+        return "springboot is running";
+    }
+}
